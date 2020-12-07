3350e99d678291c92734a62338370ccad59eedc6
Charles Xu <cyx2@illinois.edu>
2020-12-07 13:25:17 -0600

    Filled out distanceData function for Dijkstra

--- algorithms/Dijkstra.hpp
+++ algorithms/Dijkstra.hpp
@@ -13,7 +13,7 @@ namespace Dijkstra {
      * @return shortest distance between two points as a double
      */
     template <class Vertex>    
-    double getDistanceBetweenPoints(Vertex source, Vertex sink) {
+    double getDistanceBetweenPoints(Graph<Vertex>& g_, Vertex source, Vertex sink) {
         return -1.0;
     }
 
@@ -22,17 +22,62 @@ namespace Dijkstra {
      * @return shortest path between two points as a vector of Vertex objects
      */
     template <class Vertex>    
-    std::vector<Vertex> getPathBetweenPoints(Vertex source, Vertex sink) {
+    std::vector<Vertex> getPathBetweenPoints(Graph<Vertex>& g_, Vertex source, Vertex sink) {
         return std::vector<Vertex>();
     }
 
     /**
      * Uses Dijkstra's algorithm to get the distances of ALL vertices from the given point 
-     * @return unordered_map where the key is the Vertex and the value is the corresponding distance from the source
+     * @return unordered_map where the key is the Vertex and the value is a pair of the corresponding distance from the source and the previous vertex
+     * If source vertex isn't in the graph, returns an empty map
      */
     template <class Vertex>    
-    std::unordered_map<Vertex, std::pair<double, Vertex>> getDistanceDataForVertex(Vertex source) {
-        
-        return std::unordered_map<Vertex, std::pair<double, Vertex>>();
+    std::unordered_map<Vertex, std::pair<double, Vertex>> getDistanceDataForVertex(Graph<Vertex>& g_, Vertex source) {
+        std::unordered_map<Vertex, std::pair<double, Vertex>> distances;
+        if (std::find(g_.getVertices().begin(), g_.getVertices().end(), source) == g_.getVertices().end()) {
+            //if the source isn't in the graph, return empty map
+            return distances;
+        }
+        std::vector<Vertex> vertices = g_.getVertices();
+        std::unordered_map<Vertex, double> not_visited; //key is Vertex, value is current distance to source (to make it easier to check for next node to visit)
+        for (Vertex v : vertices) {
+            //set up distances
+            not_visited.insert(std::pair<v, -1.0>);
+            distances.insert(std::pair<v, std::pair<-1.0, source>>);
+        }
+        Vertex current = source;
+        auto iter = distances.find(current);
+        iter->first = 0.0;
+        while (not_visited.size() != 0) {
+            //Update weights for nodes adjacent to current
+            //Find unvisited node with next smallest distance, set to current
+
+            iter = distances.find(current);
+            std::vector<std::pair<Vertex, double>> adjacent = g_.getAdjacentWeighted(current);
+            for (std::pair<Vertex, double> adj : adjacent) {
+                Edge<Vertex> currentEdge = g_.getEdge(current, adj.first);
+                if ((adj.second == -1.0 || adj.second > iter.second + currentEdge.getWeight()) && not_visited.find(adj.first) != not_visited.end()) {
+                    //if current distance is -1 (hasn't been visited yet) or is longer than the path from current vector, update the distance
+                    not_visited[adj.first] = iter.second + currentEdge.getWeight();
+                    distances[adj.first].first = iter.second + currentEdge.getWeight();
+                    distances[adj.first].second = current;
+                }
+            }
+
+            double nextShortest;
+            Vertex nextVertex = current;
+
+            for (std::pair<Vertex, double> v : not_visited) {
+                if (nextVertex == current || v.second < nextShortest) {
+                    nextVertex = v.first;
+                    nextShortest = v.second;
+                }
+            }
+
+            not_visited.erase(current);
+            current = nextVertex;
+
+        }
+        return distances;
     }
 }
