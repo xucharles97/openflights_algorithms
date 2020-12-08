7ff743222fb65f87ed6b5b7fe760577475fd1f71
Charles Xu <cyx2@illinois.edu>
2020-12-08 01:25:09 -0600

    Added basic test for Dijkstra, moving to EWS for debugging

--- algorithms/Dijkstra.hpp
+++ algorithms/Dijkstra.hpp
@@ -5,8 +5,76 @@
 #include <vector>
 #include <unordered_map>
 #include <utility>
+#include <iostream>
 
 namespace Dijkstra {
+
+    /**
+     * Uses Dijkstra's algorithm to get the distances of ALL vertices from the given point 
+     * @return unordered_map where the key is the Vertex and the value is a pair of the corresponding distance from the source and the previous vertex
+     * If source vertex isn't in the graph, returns an empty map
+     */
+    template <class Vertex>    
+    std::unordered_map<Vertex, std::pair<double, Vertex>> getDistanceDataForVertex(Graph<Vertex>& g_, Vertex source) {
+        std::unordered_map<Vertex, std::pair<double, Vertex>> distances;
+        if (std::find(g_.getVertices().begin(), g_.getVertices().end(), source) == g_.getVertices().end()) {
+            //if the source isn't in the graph, return empty map
+            return distances;
+        }
+        std::cout << "Line " << __LINE__ << std::endl;
+        std::vector<Vertex> vertices = g_.getVertices();
+        std::unordered_map<Vertex, double> not_visited; //key is Vertex, value is current distance to source (to make it easier to check for next node to visit)
+        for (Vertex v : vertices) {
+            //set up distances
+            not_visited.insert(std::make_pair(v, -1.0));
+            distances.insert(std::make_pair(v , std::make_pair(-1.0, source)));
+        }
+        std::cout << "Line " << __LINE__ << std::endl;
+
+        if (distances.find(source) == distances.end()) {
+            return std::unordered_map<Vertex, std::pair<double, Vertex>>();
+        }
+        std::cout << "Line " << __LINE__ << std::endl;
+        Vertex current = source;
+        distances[current].first = 0.0;
+        while (not_visited.size() != 0) {
+            //Update weights for nodes adjacent to current
+            //Find unvisited node with next smallest distance, set to current
+            std::cout << "Line " << __LINE__ << std::endl;
+            
+            for (std::pair<Vertex, double> n : not_visited) {
+                std::cout << n.first;
+            }
+            std::cout << std::endl;
+            std::vector<std::pair<Vertex, double>> adjacent = g_.getAdjacentWeighted(current);
+            double currentDistance = distances[current].first;
+
+            for (std::pair<Vertex, double> adj : adjacent) {
+                Edge<Vertex> currentEdge = g_.getEdge(current, adj.first);
+                if ((distances[adj.first].first == -1.0 || distances[adj.first].first > currentDistance + currentEdge.getWeight()) && not_visited.find(adj.first) != not_visited.end()) {
+                    //if current distance is -1 (hasn't been visited yet) or is longer than the path from current vector, update the distance
+                    not_visited[adj.first] = currentDistance + currentEdge.getWeight();
+                    distances[adj.first].first = currentDistance + currentEdge.getWeight();
+                    distances[adj.first].second = current;
+                }
+            }
+
+            double nextShortest;
+            Vertex nextVertex = current;
+
+            for (std::pair<Vertex, double> v : not_visited) {
+                if (v.second < nextShortest) {
+                    nextVertex = v.first;
+                    nextShortest = v.second;
+                }
+            }
+
+            not_visited.erase(current);
+            current = nextVertex;
+
+        }
+        return distances;
+    }
     
     /**
      * Uses Dijkstra's algorithm to get the distance of the shortest path between two points
@@ -14,12 +82,13 @@ namespace Dijkstra {
      */
     template <class Vertex>    
     double getDistanceBetweenPoints(Graph<Vertex>& g_, Vertex source, Vertex sink) {
-        std::unordered_map<Vertex, std::pair<double, Vertex>> distances = getDistanceDataForVertex(g_, source);
-        std::pair<double, Vertex> data = distances.find(sink);
-        if (data == distances.end()) {
+        std::cout << "Line " << __LINE__ << std::endl;
+        std::unordered_map<Vertex, std::pair<double, Vertex>> distances = Dijkstra::getDistanceDataForVertex(g_, source);
+        std::cout << "Line " << __LINE__ << std::endl;
+        if (distances.find(sink) == distances.end()) {
             return -1.0;
         }
-        return data.first;
+        return distances[sink].first;
     }
 
     /**
@@ -44,58 +113,5 @@ namespace Dijkstra {
         return path;
     }
 
-    /**
-     * Uses Dijkstra's algorithm to get the distances of ALL vertices from the given point 
-     * @return unordered_map where the key is the Vertex and the value is a pair of the corresponding distance from the source and the previous vertex
-     * If source vertex isn't in the graph, returns an empty map
-     */
-    template <class Vertex>    
-    std::unordered_map<Vertex, std::pair<double, Vertex>> getDistanceDataForVertex(Graph<Vertex>& g_, Vertex source) {
-        std::unordered_map<Vertex, std::pair<double, Vertex>> distances;
-        if (std::find(g_.getVertices().begin(), g_.getVertices().end(), source) == g_.getVertices().end()) {
-            //if the source isn't in the graph, return empty map
-            return distances;
-        }
-        std::vector<Vertex> vertices = g_.getVertices();
-        std::unordered_map<Vertex, double> not_visited; //key is Vertex, value is current distance to source (to make it easier to check for next node to visit)
-        for (Vertex v : vertices) {
-            //set up distances
-            not_visited.insert(std::make_pair<v, -1.0>);
-            distances.insert(std::make_pair<v, std::make_pair<-1.0, source>>);
-        }
-        Vertex current = source;
-        auto iter = distances.find(current);
-        iter->first = 0.0;
-        while (not_visited.size() != 0) {
-            //Update weights for nodes adjacent to current
-            //Find unvisited node with next smallest distance, set to current
-
-            iter = distances.find(current);
-            std::vector<std::pair<Vertex, double>> adjacent = g_.getAdjacentWeighted(current);
-            for (std::pair<Vertex, double> adj : adjacent) {
-                Edge<Vertex> currentEdge = g_.getEdge(current, adj.first);
-                if ((adj.second == -1.0 || adj.second > iter.second + currentEdge.getWeight()) && not_visited.find(adj.first) != not_visited.end()) {
-                    //if current distance is -1 (hasn't been visited yet) or is longer than the path from current vector, update the distance
-                    not_visited[adj.first] = iter.second + currentEdge.getWeight();
-                    distances[adj.first].first = iter.second + currentEdge.getWeight();
-                    distances[adj.first].second = current;
-                }
-            }
-
-            double nextShortest;
-            Vertex nextVertex = current;
-
-            for (std::pair<Vertex, double> v : not_visited) {
-                if (nextVertex == current || v.second < nextShortest) {
-                    nextVertex = v.first;
-                    nextShortest = v.second;
-                }
-            }
-
-            not_visited.erase(current);
-            current = nextVertex;
-
-        }
-        return distances;
-    }
+    
 }
--- tests/Dijkstra_Tests.cpp
+++ tests/Dijkstra_Tests.cpp
@@ -1,1 +1,32 @@
 #include "../algorithms/Dijkstra.hpp"
+#include "../graph/Graph.h"
+#include "../graph/Edge.h"
+
+#include "../catch/catch.hpp"
+#include <vector>
+#include <iostream>
+
+TEST_CASE("DIJKSTRA DISTANCE IN SINGLE EDGE GRAPH", "[Dijkstra]") {
+    std::vector<Edge<std::string>> edges;
+
+    edges.push_back(Edge<std::string>("a", "b"));
+    // edges.push_back(Edge<std::string>("b", "c"));
+    // edges.push_back(Edge<std::string>("c", "d"));
+    // edges.push_back(Edge<std::string>("a", "d"));
+
+    Graph<std::string> graph(edges);
+    // std::vector<std::string> vertices = graph.getVertices();
+    // for (std::string str : vertices) {
+    //     std::cout << str << std::endl;
+    // }
+
+    // std::vector<Edge<std::string>> edgelist = graph.getEdges();
+    // for (Edge<std::string> edge : edgelist) {
+    //     std::cout << edge.source << " to " << edge.dest << std::endl;
+    // }
+    double distance = Dijkstra::getDistanceBetweenPoints<std::string>(graph, "a", "b");
+
+    REQUIRE(distance == 1.0);
+
+
+}
