47766d7c56c79b694fd23ef7572904da53134730
Madhav Aggarwal <madhav.taggarwal@gmail.com>
2020-12-09 02:08:40 +0100

    Fix FloydWarshall bug for path and add some tests

--- tests/Floyd_Warshall_Tests.cpp
+++ tests/Floyd_Warshall_Tests.cpp
@@ -2,6 +2,9 @@
 // Created by Madhav on 12/06/20.
 //
 
+#include <iostream>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "../graph/Edge.h"
@@ -11,8 +14,69 @@
 
 #include "../catch/catch.hpp"
 
+using std::string;
+using std::unordered_map;
+using std::vector;
+
 using FloydWarshall::shortestDistanceBetweenAllVertices;
 using FloydWarshall::shortestPathBetweenAllVertices;
 using FloydWarshall::shortestPathBetweenTwoVertices;
 
-TEST_CASE("Shortest Distance Basic", "[Distance]") {}
+Graph<string> makeBasicGraph() {
+    vector<Edge<string>> strEdges;
+    strEdges.push_back(Edge<string>("a", "b", 5));
+    strEdges.push_back(Edge<string>("b", "d", 1));
+    strEdges.push_back(Edge<string>("d", "c", 2));
+    strEdges.push_back(Edge<string>("b", "c", 5));
+    strEdges.push_back(Edge<string>("c", "b", 5));
+
+    Graph<string> stringGraph(strEdges);
+
+    return stringGraph;
+}
+
+TEST_CASE("Shortest Distance Basic", "[Distance]") {
+    Graph<string> stringGraph = makeBasicGraph();
+
+    unordered_map<string, unordered_map<string, double>> shortestDistances =
+        shortestDistanceBetweenAllVertices(stringGraph);
+
+    REQUIRE(shortestDistances["a"]["a"] == 0);
+    REQUIRE(shortestDistances["a"]["c"] == 8);
+    REQUIRE(shortestDistances["d"]["b"] == 7);
+}
+
+TEST_CASE("Shortest Path Between Two Basic #1", "[Path]") {
+    Graph<string> stringGraph = makeBasicGraph();
+
+    std::vector<string> shortestPath =
+        shortestPathBetweenTwoVertices<string>(stringGraph, "a", "a");
+
+    // From A to A
+    REQUIRE(shortestPath[0] == "a");
+}
+
+TEST_CASE("Shortest Path Between Two Basic #2", "[Path]") {
+    Graph<string> stringGraph = makeBasicGraph();
+
+    std::vector<string> shortestPath =
+        shortestPathBetweenTwoVertices<string>(stringGraph, "a", "c");
+
+    // From A to C
+    REQUIRE(shortestPath[0] == "a");
+    REQUIRE(shortestPath[1] == "b");
+    REQUIRE(shortestPath[2] == "d");
+    REQUIRE(shortestPath[3] == "c");
+}
+
+TEST_CASE("Shortest Path Between Two Basic #3", "[Path]") {
+    Graph<string> stringGraph = makeBasicGraph();
+
+    std::vector<string> shortestPath =
+        shortestPathBetweenTwoVertices<string>(stringGraph, "d", "b");
+
+    // From D to B
+    REQUIRE(shortestPath[0] == "d");
+    REQUIRE(shortestPath[1] == "c");
+    REQUIRE(shortestPath[2] == "b");
+}
