1a16833ef3406a5cbe62e639eb3f1b08cb9cf5c3
Matt Geimer <mgeimer2@illinois.edu>
2020-12-10 14:19:49 -0600

    <feat> initial drive code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "graph/Graph.h"
 #include "graph/Edge.h"
+#include "graph/Parsing.hpp"
 
-int main()
-{
-
-    return 0;
+int main() {
+    std::string airportFile = "datasets/airports.txt";
+    std::string routeFile = "datasets/routes.txt";
+    Graph<std::string> graph = Parsing::buildGraphFromFiles<std::string>(airportFile, routeFile);
 }
