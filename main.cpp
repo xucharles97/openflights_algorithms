9848a22b369f4df5d5a23dd406eadd676f77eae0
Charles Xu <cyx2@illinois.edu>
2020-12-10 17:07:32 -0600

    Updated main to run Dijkstra on random airport

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "algorithms/BetweenessCentrality.hpp"
+#include "algorithms/Dijkstra.hpp"
 #include "graph/Edge.h"
 #include "graph/Graph.h"
 #include "graph/Parsing.hpp"
 #include <iostream>
+#include <random>
 
 int main() {
     std::string airportFile = "datasets/airports.txt";
@@ -16,6 +18,21 @@ int main() {
     std::cout << "Finished building graph! There were " << graph.getVertices().size()
               << " airports and " << graph.getEdges().size() << " routes between them."
               << std::endl;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, graph.getVertices().size() - 1);
+
+    std::string dijkstraSource = graph.getVertices()[dis(gen)];
+
+    std::cout << "Running Dijkstra's Algorithm for airport " << dijkstraSource
+              << std::endl;
+    
+    Dijkstra::printDijkstraData(graph, dijkstraSource);
+    std::cout << "Finished running Dijkstra's Algorithm for airport " << dijkstraSource
+              << std::endl;
+    Dijkstra::printShortestDijkstraDistance(graph, dijkstraSource, graph.getVertices()[dis(gen)]);
+    Dijkstra::printShortestDijkstraPath(graph, dijkstraSource, graph.getVertices()[dis(gen)]);
 
     std::cout << "Computing the most central airport. Hold on! This can take a while..."
               << std::endl;
